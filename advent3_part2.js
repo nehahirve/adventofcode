0f88a25333987d2e67bda8a14f47d004801abac8
nehahirve <neha.hirve@hyperisland.se>
2020-12-17 21:07:26 +0100

    day 3 part 2

---
+++ advent3_part2.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const map = fs
+  .readFileSync('input_3.txt')
+  .toString()
+  .split('\n')
+  .map(row => row.split(''))
+
+const slopes = [
+  { x: 1, y: 1 },
+  { x: 3, y: 1 },
+  { x: 5, y: 1 },
+  { x: 7, y: 1 },
+  { x: 1, y: 2 }
+]
+
+let product = 1
+
+for (let slope of slopes) {
+  product *= countTrees(slope)
+}
+
+console.log(product)
+
+function countTrees(slope) {
+  let trees = 0
+  let pos = { x: 0, y: 0 }
+  for (let i = slope.y; i < map.length; i += slope.y) {
+    pos.x = (pos.x + slope.x) % map[0].length
+    pos.y = i
+    console.log(pos)
+    if (map[pos.y][pos.x] === '#') {
+      trees++
+    }
+  }
+  return trees
+}
